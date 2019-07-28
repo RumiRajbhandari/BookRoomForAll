1d21dedd383fc7dcf50dcb999b31c37ddc4e4025
Rumi Rajbhandari <rumirajbhandari@gmail.com>
2019-07-28 16:13:21 +0545

    python image library added

--- Dataloader.py
+++ Dataloader.py
@@ -2,8 +2,10 @@ import numpy as np
 
 def load_data():
     dataset = np.load('dataset/dataset.npz')
+    my_dict = {1: 'क', 2: 'ख', 3: 'ग', 4: 'घ', 5: 'ङ', 6:'च', 7: 'छ', 8:'ज',9:'झ',10:'ञ',11:'ट',12:'ठ',13:'ड',14:'ढ',15:'ण', 16:'त',17:'थ',18:'द', 19:'ध',20:'न',21:'प',22:'फ',23:'ब',24:'भ',25:'म', 26:'य', 27:'र', 28:'ल', 29:'व', 30:'श', 31:'ष', 32:'स', 33:'ह', 34:'क्ष', 35:'त्र', 36:'ज्ञ', 37:'0', 38:'1', 39:'2', 40:'3', 41:'4', 42:'5', 43:'6', 44:'7', 45:'8', 46:'9' }
+
     train_label = dataset['arr_1']
     train_image = dataset['arr_0']
     test_label = dataset['arr_3']
     test_image = dataset['arr_2']
-    return (train_image, train_label), (test_image, test_label)
+    return (train_image, train_label), (test_image, test_label), my_dict
--- ImageResize.py
+++ ImageResize.py
@@ -1,8 +1,14 @@
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter,ImageOps
 import numpy as np
+import tensorflow as tf
+
+from keras.preprocessing import image
 
 def resizeImage(img):
-    im = Image.open(img)
-    resize_image = im.resize((32, 32), Image.ANTIALIAS)
-    image_arr = np.asarray(resize_image)
-    return image_arr[:,:,0]
+    test_image = image.load_img(img, target_size = (32,32), color_mode="grayscale")
+    test_image = ImageOps.invert(test_image)
+    test_image = tf.keras.utils.normalize(test_image, axis=1)
+    test_image = image.img_to_array(test_image)
+    test_image = test_image.reshape((test_image.shape[0], -1), order='F')
+    test_image = np.expand_dims(test_image, axis = 0)
+    return test_image
